90a68949347e54b1b074b54cd2df14664009dade
Mateen Ulhaq <mulhaq2005@gmail.com>
2020-12-08 17:53:53 -0800

    feat: read from websocket

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -2,6 +2,16 @@ var lineArr = [];
 var MAX_LENGTH = 100;
 var duration = 500;
 var chart = realTimeLineChart();
+var split_layer_text = document.getElementById("split_layer");
+
+var curr_dict = {
+  kb_transmitted: 0.0,
+  ms_inference: 0.0,
+  ms_roundtrip: 0.0,
+  split_layer: "unknown"
+};
+
+var ws;
 
 function randomNumberBounds(min, max) {
   return Math.floor(Math.random() * max) + min;
@@ -24,10 +34,11 @@ function updateData() {
 
   var lineData = {
     time: now,
-    kb_transmitted: randomNumberBounds(0, 2.5),
-    ms_inference: randomNumberBounds(0, 5),
-    ms_roundtrip: randomNumberBounds(0, 10)
+    kb_transmitted: curr_dict["kb_transmitted"],
+    ms_inference: curr_dict["ms_inference"],
+    ms_roundtrip: curr_dict["ms_roundtrip"]
   };
+
   lineArr.push(lineData);
 
   if (lineArr.length > 30) {
@@ -44,11 +55,22 @@ function resize() {
   d3.select("#chart").call(chart);
 }
 
+function connectWebsocket() {
+  ws = new WebSocket("ws://ensc-mcl-28.engineering.sfu.ca:8765");
+  ws.onmessage = (event) => {
+    curr_dict = JSON.parse(event.data);
+    console.log(curr_dict);
+    split_layer_text.textContent = curr_dict["split_layer"];
+  };
+  // window.setInterval(() => {ms_inference += 1}, 2000);
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   seedData();
   window.setInterval(updateData, 500);
   d3.select("#chart").datum(lineArr).call(chart);
   d3.select(window).on('resize', resize);
+  connectWebsocket();
 });
 
 // Inference time, total inference time, ...
--- demo.md
+++ demo.md
@@ -4,12 +4,21 @@ layout: default
 
 ## ColliFlow: A Library for Executing Collaborative Intelligence Graphs
 
+<!-- <link rel="stylesheet" href="demo.css"> -->
+
 <hr />
 
 <script type="text/javascript" src="https://d3js.org/d3.v6.min.js"></script>
 <!-- <script type="text/javascript" src="d3/d3.v6.min.js"></script> -->
 
 <div id="chart"></div>
+
+<span class=".info-category" style="font-weight: bold">Model:</span>
+<span id="model_name" class=".info-text">ResNet-34</span>
+
+<span class=".info-category" style="font-weight: bold">Split Layer:</span>
+<span id="split_layer" class=".info-text"></span>
+
 <script src="assets/js/realtimelinechart.js"></script>
 <script src="assets/js/demo.js"></script>
 
